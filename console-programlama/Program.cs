4c91415302391ec2a7273b47e9eff57815635280
Gazi Mert <gazimert56@gmail.com>
2022-02-08 23:44:54 +0300

    First Commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("soy isminizi giriniz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
