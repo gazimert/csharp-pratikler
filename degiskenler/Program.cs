1b242511e8a485071afc45cb23627f1953aba118
Gazi Mert <gazimert56@gmail.com>
2022-02-09 13:36:49 +0300

    First Commit

---
+++ degiskenler/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 5;      // 1 byte
+            sbyte c = 5;     // 1 byte
+
+            short s = 5;     // 2 byte
+            ushort us = 5;   // 2 byte
+
+            Int16 i16 = 2;   // 2 byte
+            int i = 2;       // 4 byte   
+            Int32 i32 = 2;   // 4 byte
+            Int64 i64 = 2;   // 8 byte
+
+            uint ui = 4;     // 4 byte
+            long l = 4;      // 8 byte
+            ulong ul = 4;    // 8 byte
+            
+            // Reel Sayılar
+            float f = 3;     // 4 byte
+            double d = 3.4;  // 8 byte
+            decimal de = 12; // 16 byte
+
+            char ch = '2';      // 2 byte
+            string str = "as";  //sınırsız
+
+            bool b1 = true;
+            bool b2 = false;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "a";
+            object o2 = '2';
+            object o3 = 4;
+            object o4 = 4.5;
+
+            // String ifadeler
+
+            string str1 = string.Empty;
+            str1 = "Gazi Mert";
+            string ad = "Gazi";
+            string soyad = " Mert";
+            string tamisim = ad + " " + soyad;
+            
+
+            //integer tanımlama şekilleri
+
+            int integer1 = 5;
+            int integer2 = 2;
+            int integer3 = integer1 * integer2;
+
+
+            // boolen
+
+            bool bool1 = 10 < 2;
+
+            // Değişken Dönüşümleri
+
+            string str20 = "20";
+            int int20 = 20;
+
+            string yenideger = str20 + int20.ToString();
+
+            Console.WriteLine(yenideger); // 2020
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21);     // 40
+
+            int int22 = int20 + int.Parse(str20); // 40
+
+
+            // Datetime 
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+
+            Console.WriteLine(datetime);
+
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+
+            Console.WriteLine(datetime2);
+
+            string hour = DateTime.Now.ToString("HH:mm");
+
+            Console.WriteLine(hour);
+        }
+    }
+}
