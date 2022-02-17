c136dba33804db886fd80b2df09b914a3a99d565
Gazi Mert <gazimert56@gmail.com>
2022-02-18 00:08:38 +0300

    First Commit

---
+++ odev-1/soru-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace soru_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
+            // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+
+            Console.Write("Bir cümle giriniz: ");
+
+            string cumle = Console.ReadLine();
+            
+            Console.WriteLine();
+
+            int harfSayac = 0, kelimeSayac = 1;
+
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (cumle[i] == ' '){
+                    kelimeSayac++;
+                    continue;
+                }
+                harfSayac++;
+            }
+
+            Console.WriteLine("Kelime Sayısı: " + kelimeSayac);
+            Console.WriteLine("Harf Sayısı: " + harfSayac);
+        }
+    }
+}
